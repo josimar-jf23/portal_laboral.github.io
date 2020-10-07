964d9ef2c752e412d5ef1e721788438107e96eb6
âJosimar Aldahir Jauregui <josimar.jauregui.negocios@gmail.com>
2020-10-06 23:41:21 -0500

    dashboar de prueba

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -7,54 +7,17 @@
 @stop
 
 @section('content')
+<div class="row">
+    <h2>Dashboards</h2>
+</div>
     <div class="row">
         <div class="col-md-6 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading"><b>Charts</b></div>
+                <div class="panel-heading"><b>TOTAL DE SUSCRIPTOS POR MES</b></div>
                 <div class="panel-body">
-                    <canvas id="myChart" height="100" width="400"></canvas>
+                    <canvas id="myChart1" height="100" width="400"></canvas>
                 </div>
             </div>
-        </div>
-        <div class="col-md-6 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading"><b>Charts</b></div>
-                <div class="panel-body">
-                    <canvas id="myChart" height="100" width="400"></canvas>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-header">
-            <h1>Dashboard</h1>
-        </div>
-        <div class="card-body">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid soluta nemo deserunt, nihil illum ipsum sapiente pariatur? Molestias, at ab praesentium ratione et eos accusamus corporis similique rem deleniti ipsum.</p>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-header">
-            <h1>Dashboard</h1>
-        </div>
-        <div class="card-body">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid soluta nemo deserunt, nihil illum ipsum sapiente pariatur? Molestias, at ab praesentium ratione et eos accusamus corporis similique rem deleniti ipsum.</p>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-header">
-            <h1>Dashboard</h1>
-        </div>
-        <div class="card-body">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid soluta nemo deserunt, nihil illum ipsum sapiente pariatur? Molestias, at ab praesentium ratione et eos accusamus corporis similique rem deleniti ipsum.</p>
-        </div>
-    </div>
-    <div class="card">
-        <div class="card-header">
-            <h1>Dashboard</h1>
-        </div>
-        <div class="card-body">
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid soluta nemo deserunt, nihil illum ipsum sapiente pariatur? Molestias, at ab praesentium ratione et eos accusamus corporis similique rem deleniti ipsum.</p>
         </div>
     </div>
 @stop
@@ -66,32 +29,62 @@
 @section('js')
     <script> console.log('Hi!'); </script>
     <script>
-        var ctx = document.getElementById('myChart');
+        var ctx = document.getElementById('myChart1');
         var myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-                datasets: [{
-                    label: '# of Votes',
-                    data: [12, 19, 3, 5, 2, 3],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                labels: [
+                    @foreach ($contador_suscripciones as $r)
+                    
+                    @php
+                        $nom_mes='';
+                        switch( $r->meses ) {
+                            case '01': $nom_mes= 'Enero'; break;case '02': $nom_mes= 'Febrero'; break;case '03': $nom_mes= 'Marzo'; break;
+                            case '04': $nom_mes= 'Abril'; break;case '05': $nom_mes= 'Mayo'; break;case '06': $nom_mes= 'Junio'; break;
+                            case '07': $nom_mes= 'Julio'; break;case '08': $nom_mes= 'Agosto'; break;case '09': $nom_mes= 'Setiembre'; break;
+                            case '10': $nom_mes= 'Octubre'; break;case '11': $nom_mes= 'Noviembre'; break;case '12': $nom_mes= 'Diciembre'; break;
+                        }
+                    @endphp
+                    ' {{ $nom_mes }} - {{$r->anios}}',
+                    @endforeach
                     ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                }]
+                datasets: [
+                    @foreach ($contador_suscripciones as $r)
+                        {                        
+                            label: 
+                                @php
+                                    $nom_mes='';
+                                    switch( $r->meses ) {
+                                        case '01': $nom_mes= 'Enero'; break;case '02': $nom_mes= 'Febrero'; break;case '03': $nom_mes= 'Marzo'; break;
+                                        case '04': $nom_mes= 'Abril'; break;case '05': $nom_mes= 'Mayo'; break;case '06': $nom_mes= 'Junio'; break;
+                                        case '07': $nom_mes= 'Julio'; break;case '08': $nom_mes= 'Agosto'; break;case '09': $nom_mes= 'Setiembre'; break;
+                                        case '10': $nom_mes= 'Octubre'; break;case '11': $nom_mes= 'Noviembre'; break;case '12': $nom_mes= 'Diciembre'; break;
+                                    }
+                                @endphp
+                                ' {{ $nom_mes }} - {{$r->anios}}'
+                            ,
+                            data: [{{ $r->total }}],
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255, 99, 132, 1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }
+                    @endforeach
+                    
+                    ]
             },
             options: {
                 scales: {
